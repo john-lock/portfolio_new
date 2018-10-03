86c6970f4ea6feaf66657d949871a7e025b15c9b
jl <jonathan.arthur.lock@gmail.com>
2018-10-03 09:52:39 +0200

    cms CRuD, added db
    pre user auth

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,21 @@
 from app import app
 from flask import render_template
-from flask import Flask, request, redirect, url_for
+from flask import request, redirect, url_for
 from flask_mail_sendgrid import MailSendGrid
 from flask_mail import Message
+from pf import Projects, db
+
 
 app.config['MAIL_SENDGRID_API_KEY'] = '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'
 mail = MailSendGrid(app)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    myprojects = Projects.query.filter_by(show=True).all()
+    return render_template('index.html', myprojects=myprojects)
+
 
 @app.route('/contact', methods=['POST'])
 def contact():
@@ -24,3 +29,34 @@ def contact():
         msg.html = fullmsg
         mail.send(msg)
     return redirect('index')
+
+
+@app.route('/projects')
+def projects():
+    myprojects = Projects.query.filter_by(show=True).all()
+    return render_template('projects.html', myprojects=myprojects)
+
+
+@app.route('/project/add', methods=['POST'])
+def project_add():
+    if request.method == 'POST':
+        newproject = Projects(
+        name=request.form['name'],
+        card_text=request.form['card_text'],
+        modal_body=request.form['modal_body'],
+        modal_short=request.form['modal_short'],
+        modal_tech=request.form['modal_tech'],
+        show=True
+        )
+        db.session.add(newproject)
+        db.session.commit()
+    return redirect('projects')
+
+
+@app.route('/projects/delete/<id>', methods=['GET'])
+def project_delete(id):
+    if request.method == 'GET':
+        project = Projects.query.filter_by(id=id).first()
+        db.session.delete(project)
+        db.session.commit()
+    return redirect('projects')
--- pf.py
+++ pf.py
@@ -1,1 +1,18 @@
 from app import app
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'portfolioprojects.db')
+db = SQLAlchemy(app)
+
+class Projects(db.Model):
+    __tablename__ = "projects"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    show = db.Column(db.Boolean, default=True)
+    card_text = db.Column(db.String(400))
+    modal_body = db.Column(db.TEXT)
+    modal_short = db.Column(db.TEXT)
+    modal_tech = db.Column(db.String(400))
